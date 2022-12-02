92109711dafc3e5859cc8ec2ef18025ed61dd32e
Denis Tkachev <allpost-s@ya.ru>
2022-12-02 19:23:17 +0300

    Task7 is finished

--- task7/Program.cs
+++ task7/Program.cs
@@ -1,5 +1,4 @@
 ﻿// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа. 456 -> 6; 782 -> 2; 918 -> 8
 Console.WriteLine("Введите трехзначное число: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int[] array = {N};
-Console.Write(array[1]);
+string N = Console.ReadLine();
+Console.Write(N[2]);
