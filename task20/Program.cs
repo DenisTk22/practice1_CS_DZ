a16e356d917123fa790cec7f725eace62528d1cb
Denis Tkachev <allpost-s@ya.ru>
2022-12-06 22:07:30 +0300

    Добавил задачи 17, 18, 20, 22

---
+++ task20/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 20: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите координаты точки А по X и Y: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B по X и Y: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+int dif1 = (x1 - x2);
+int dif2 = (y1 - y2);
+double dist = Math.Sqrt(Math.Pow(dif1, 2) + Math.Pow(dif2, 2));
+Console.WriteLine($"Расстояние между точками A и B = {dist:f2}");
