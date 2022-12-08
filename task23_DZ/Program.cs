91c4b8f88eb071b6a72139c7dbd1dbeb81b66f2a
Denis Tkachev <allpost-s@ya.ru>
2022-12-08 17:29:45 +0300

    подшаманил

--- task23_DZ/Program.cs
+++ task23_DZ/Program.cs
@@ -4,5 +4,5 @@ Console.WriteLine("Введите число N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 for ( int i = 1; i <= n; i++)
 {
-    Console.Write($"{Math.Pow(i, 3)}, ");
+    Console.Write($"{Math.Pow(i, 3)} ");
 }
