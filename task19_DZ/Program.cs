ea37fc43aaa78cdb2eacf16d120c08f4254413ad
Denis Tkachev <allpost-s@ya.ru>
2022-12-08 17:24:44 +0300

    Сохранение задачи из ДЗ task19_DZ

---
+++ task19_DZ/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 14212 -> нет; 12821 -> да; 23432 -> да
+
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int mynum = num;
+int revN = 0;
+while (num > 0)
+{
+    revN = revN * 10 + num % 10;
+    num = num / 10;
+}
+
+if (mynum - revN == 0)
+{
+    Console.WriteLine("Ваше число палиндром");
+}
+else 
+{
+    Console.WriteLine("Число как число");
+}
