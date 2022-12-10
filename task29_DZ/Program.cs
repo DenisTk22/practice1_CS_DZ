fa00028ee976278e22c8bcd78ef6c5e2984a1b6e
Denis Tkachev <allpost-s@ya.ru>
2022-12-10 21:35:32 +0300

    сделал задачу 29 из дз

---
+++ task29_DZ/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+
+int ReadInt(string massage)
+{
+    Console.WriteLine(massage); 
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int [] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = ReadInt($"Введите {i+1} элемент из 8");
+}
+
+Console.WriteLine(string.Join(", " , array));
