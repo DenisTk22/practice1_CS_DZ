95a77c228430f338ab9cd55cad4802d6c5ccb5c3
Denis Tkachev <allpost-s@ya.ru>
2022-12-15 21:08:18 +0300

    сделал задачи 36 и 38 из дз

---
+++ task36_DZ/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19; [-4, -6, 89, 6] -> 0
+
+int ReadInt(string massage)
+{
+    Console.WriteLine(massage);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GetRandomArray(int length) // задание метода для случайного массива
+{
+    int[] array = new int[length];
+    for(int i=0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+    }
+    return array;
+}
+
+int sumChet(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if (i % 2 != 0)
+        {
+            sum = sum +array[i];
+        }
+    }
+    return sum;
+}
+
+int l = ReadInt("Введите длину массива: ");
+
+int[] massive = GetRandomArray(l);
+Console.WriteLine($"Случайный массив из {l} элементов: {string.Join(", ", massive)}");
+
+int sumCh = sumChet(massive);
+Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях = {sumCh}");
+
---
+++ task38_DZ/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 38: Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int ReadInt(string massage)
+{
+    Console.WriteLine(massage);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GetRandomArray(int length) // задание метода для случайного массива
+{
+    int[] array = new int[length];
+    for(int i=0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+
+(int, int, int) sumChet(int[] array)
+{
+    int indexmin = 0;
+    int indexmax = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if (array[indexmax] < array[i])
+        {
+            indexmax = i;
+        }
+    else if (array[indexmin] > array[i])
+       {
+            indexmin = i;
+       }
+    }
+    int diff = array[indexmax] - array[indexmin];
+    return (array[indexmax], array[indexmin], diff);
+}
+
+int l = ReadInt("Введите длину массива: ");
+
+int[] massive = GetRandomArray(l);
+Console.WriteLine($"Случайный массив из {l} элементов: {string.Join(", ", massive)}");
+
+(int max, int min, int diffCh) = sumChet(massive);
+Console.WriteLine($"Разница между максимальным элементом <{max}> и минимальным элементом <{min}> данного массива = {diffCh}");
